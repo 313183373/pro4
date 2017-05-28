7f750a11871e8654b27aed488104a28078a175ed
star xiong <313183373@qq.com>
2017-05-28 19:38:14 +0800

    1

--- main/main.js
+++ main/main.js
@@ -1,18 +1,39 @@
 module.exports = function main() {
     let sequence = new Sequence([6, 9, 15, -2, 92, 11]);
-    console.log(`o) 最小值 = ${sequence.minimum()}
-// Write your code here
-`);
+    console.log(`o) 最小值 = ${sequence.minimum()}\no) 最大值 = ${sequence.maximum()}\no) 平均值 = ${sequence.average()}`);
 };
 
 class Sequence {
-  constructor(input) {
-    // Write your code here
-  }
+	constructor(input) {
+		this.input=input;
+	}
 
-  minimum() {
-    // Write your code here
-  }
+	minimum() {
+		var min=this.input[0];
+    	for(let i=0;i<this.input.length;i++){
+    		if(min>input[i]){
+    			min=input[i];
+    		}
+    	}
+    	return min;
+  	}
 
-  // Write your code here
+  	maximun(){
+  		var max=this.input[0];
+    	for(let i=0;i<this.input.length;i++){
+    		if(max<input[i]){
+    			max=input[i];
+    		}
+    	}
+    	return max;
+  	}
+
+  	average(){
+  		var sum=0;
+  		for(let i=0;i<this.input.length;i++){
+  			sum+=input[i];
+  		}
+  		var aver=sum/this.input.length;
+  		return aver.toFixed(2);
+  	}
 }
